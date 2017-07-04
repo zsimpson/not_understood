bfd5488bb1abadc883598c50735769614ffc3c08
Ubuntu <ubuntu@ip-172-31-59-91.ec2.internal>
2017-07-04 20:36:02 +0000

    WIP

--- pluck_articles.py
+++ pluck_articles.py
@@ -2,17 +2,20 @@ from plumbum import local
 import json
 import random
 
+# PLUCK the control files randomly
+
 input_folder = 'generated_data/extracted_articles'
 output_path = 'generated_data/control_articles.jsoncr'
 results_file = open(output_path, 'w')
 
-article_count = 10
+article_count = 34175
 
 output = open(output_path, 'w')
 
-# PLUCK the control files randomly
 fps = [fp for fp in (local.cwd / input_folder).walk() if fp.is_file()]
 for i in xrange(0,article_count):
+    if i % 100 == 0:
+        print i
     file_i = random.randint(0, len(fps))
     f = fps[file_i].open()
     lines = f.readlines()
