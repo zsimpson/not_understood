85a2754a11a76f03f843e619ac8abd498919f7f3
Zack Simpson <zack@vium.com>
2017-07-04 13:40:02 -0700

    wip

--- pluck_articles.py
+++ pluck_articles.py
@@ -1,6 +1,7 @@
 from plumbum import local
 import json
 import random
+import sys
 
 # PLUCK the control files randomly
 
@@ -8,9 +9,18 @@ input_folder = 'generated_data/extracted_articles'
 output_path = 'generated_data/control_articles.jsoncr'
 results_file = open(output_path, 'w')
 
+
+fps = [fp for fp in (local.cwd / input_folder).walk() if fp.is_file()]
+for i, fp in enumerate(fps):
+    print i, sum((1 for i in open(file_path, 'rb')))
+
+sys.exit()
+
 article_count = 34175
 
 output = open(output_path, 'w')
+
+files_and_lines = []
 
 fps = [fp for fp in (local.cwd / input_folder).walk() if fp.is_file()]
 for i in xrange(0,article_count):
